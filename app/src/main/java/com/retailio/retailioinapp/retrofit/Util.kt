84cb70a798b659d6a06dc09d0bafadb072edbc68
ravishrivastava <ravi.shrivastava@retailio.in>
2022-01-18 12:48:47 +0530

    manifest merger failed issue checking

--- app/src/main/java/com/retailio/retailioinapp/retrofit/Util.kt
+++ app/src/main/java/com/retailio/retailioinapp/retrofit/Util.kt
@@ -3,20 +3,13 @@ package com.retailio.retailioinapp.retrofit
 import android.util.Log
 import com.google.gson.GsonBuilder
 import com.retailio.retailioinapp.BuildConfig
-import okhttp3.OkHttpClient
 import retrofit2.HttpException
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.util.concurrent.TimeUnit
 
 private fun retrofit(): Retrofit {
 
-    val builder = OkHttpClient.Builder()
-        .readTimeout(20, TimeUnit.SECONDS)
-        .connectTimeout(10, TimeUnit.SECONDS)
-
     return Retrofit.Builder().baseUrl(BuildConfig.BASE_URL)
-        .client(builder.build())
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 }
